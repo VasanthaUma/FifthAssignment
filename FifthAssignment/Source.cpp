40424c6ea3bce36d1a33a4d1b7c5ba786919c418
VasanthaUma <buddigav@gmail.com>
2018-12-02 20:17:14 -0500

    DescriptionofCakes

--- FifthAssignment/Source.cpp
+++ FifthAssignment/Source.cpp
@@ -1,6 +1,9 @@
 /* Developed by Vasantha Buddiga 
 -  using Classes in Project */
-
+/* Display different Kinds of Cakes like Cup cakes,
+sheet cakes, wedding cakes etc. with differnt flavours 
+and toppings
+*/
 #include <iostream>
 #include <string>
 using namespace std;
